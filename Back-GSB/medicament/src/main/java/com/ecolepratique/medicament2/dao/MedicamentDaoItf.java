45e0d59952e8f0b3472b7b91cc5e4ca042196385
Diallousmane <73542636+Diallousmane@users.noreply.github.com>
2021-02-18 11:54:52 +0100

    Add files via upload

---
+++ Back-GSB/medicament/src/main/java/com/ecolepratique/medicament2/dao/MedicamentDaoItf.java
@@ -0,0 +1,10 @@
+package com.ecolepratique.medicament2.dao;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ecolepratique.medicament2.entite.Medicament;
+
+public interface MedicamentDaoItf extends JpaRepository <Medicament,Long> {
+
+}
